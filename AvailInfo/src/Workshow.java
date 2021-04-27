677d8aa233c88d35b873bae5f873d6ee973ea3a2
unknown <617713579@qq.com>
2021-04-27 21:00:00 +0800

    myfile

--- AvailInfo/src/Workshow.java
+++ AvailInfo/src/Workshow.java
@@ -14,7 +14,20 @@ public class Workshow {
                 String price = null;
                 String sales = null;
                 System.out.println("书号"+"\t\t书名" +"\t\t商店"+"\t\t价格"+"\t\t销量");
-     
+                while(rs.next()){
+                    id = rs.getInt("book_id");
+                    name = rs.getString("name");
+                    store = rs.getString("store");
+                    price=rs.getString("price");
+                    sales=rs.getString("sales");
+                    System.out.println(id+"\t\t"+name + "\t\t" + store+"\t\t"+price+"\t\t"+sales);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.out.println("查询失败，请检查数据库连接");
 
      
         }
