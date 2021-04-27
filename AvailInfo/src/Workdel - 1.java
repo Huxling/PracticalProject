7d0f7f94fe6775653b2e714c630a1faa6d967830
lxpxp <3323547983@qq.com>
2021-04-27 21:46:07 +0800

    del1.java

---
+++ AvailInfo/src/Workdel - 1.java
@@ -0,0 +1,10 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Workdel {
+    String sql;
+    Statement stmt = null;
+    Workconnect wc = new Workconnect();
+    
+}
