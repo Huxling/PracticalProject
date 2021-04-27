662b14d3dba52a046df0c931cce47705a4f845a5
unknown <617713579@qq.com>
2021-04-27 20:47:18 +0800

    myfile

--- AvailInfo/src/Workshow.java
+++ AvailInfo/src/Workshow.java
@@ -1,2 +1,22 @@
+import java.sql.*;
+
 public class Workshow {
+    String sql = "select * from book_info";
+    Statement stmt = null;
+    Workconnect wc = new Workconnect();
+    public void run(){
+        if((stmt = wc.run())!=null) {
+            try {
+                ResultSet rs = stmt.executeQuery(this.sql);
+                Integer id = null;
+                String name = null;
+                String store = null;
+                String price = null;
+                String sales = null;
+                System.out.println("书号"+"\t\t书名" +"\t\t商店"+"\t\t价格"+"\t\t销量");
+     
+
+     
+        }
+    }
 }
